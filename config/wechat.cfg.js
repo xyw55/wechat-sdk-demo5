7b98da17488c3e2b604c4116b366c4422f95dcbc
xiayiwei <yiwei.xia@ele.me>
2017-06-23 18:35:02 +0800

    first commit

---
+++ config/wechat.cfg.js
@@ -0,0 +1,9 @@
+module.exports = {
+	grant_type: 'client_credential',
+	appid: 'wxea41e4ef96cd0ac8',
+	secret: '4449461fbe4d2df6d6f67a32bedd87d7',
+	noncestr:'Wm3WZYTPz0wzccnW',
+	accessTokenUrl:'https://api.weixin.qq.com/cgi-bin/token',
+	ticketUrl:'https://api.weixin.qq.com/cgi-bin/ticket/getticket',
+	cache_duration:1000*60*60*24
+}
---
+++ router/index.js
@@ -0,0 +1,20 @@
+exports.init = function (app) {
+	var wechat_cfg = require('../config/wechat.cfg');
+	var http = require('http');
+	var cache = require('memory-cache');
+	var sha1 = require('sha1'); //签名算法
+	//var url = require('url');
+	var signature = require('../sign/signature');
+
+	
+	app.get('/',function(req,res){
+		//var url = req.protocol + '://' + req.host + req.path;
+		var url = req.protocol + '://' + req.host + req.originalUrl; //获取当前url
+		console.log(req);
+		signature.sign(url,function(signatureMap){
+			signatureMap.appId = wechat_cfg.appid;
+			res.render('index',signatureMap);
+		});
+	});
+
+};
