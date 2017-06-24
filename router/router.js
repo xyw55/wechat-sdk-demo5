a2cccd15b36cb3deabbf46abd8a72b2f88bb2343
xiayiwei <yiwei.xia@ele.me>
2017-06-24 16:43:33 +0800

    commit 21

--- router/router.js
+++ router/router.js
@@ -42,7 +42,8 @@ exports.init = function (app) {
 	                if (body.UserId) {
 						var sign_url = req.protocol + '://' + req.host + req.originalUrl; //获取当前url
 						console.log(sign_url,req.originalUrl);
-						sign_url = "http://weixin.xyw55.com/"
+						console.log("============");
+						sign_url = "http://weixin.xyw55.com";
 	                    signature.sign(sign_url, function(signatureMap){
 							signatureMap.appId = wechat_cfg.appid;
 							signatureMap.UserId = body.UserId;
