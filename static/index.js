4e9a93f034eaac0d7434a3f2c1599e58b15d1f4c
xiayiwei <yiwei.xia@ele.me>
2017-06-24 17:39:41 +0800

    commit 21

--- static/index.js
+++ static/index.js
@@ -63,57 +63,6 @@ wx.ready(function(){
     });
   };
 
-   // 2. 分享接口
-  // 2.1 监听“分享给朋友”，按钮点击、自定义分享内容及分享结果接口
-  document.querySelector('#onMenuShareAppMessage').onclick = function () {
-    wx.onMenuShareAppMessage({
-      title: '互联网之子',
-      desc: '在长大的过程中，我才慢慢发现，我身边的所有事，别人跟我说的所有事，那些所谓本来如此，注定如此的事，它们其实没有非得如此，事情是可以改变的。更重要的是，有些事既然错了，那就该做出改变。',
-      link: 'http://movie.douban.com/subject/25785114/',
-      imgUrl: 'http://demo.open.weixin.qq.com/jssdk/images/p2166127561.jpg',
-      trigger: function (res) {
-        // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
-        alert('用户点击发送给朋友');
-      },
-      success: function (res) {
-        alert('已分享');
-      },
-      cancel: function (res) {
-        alert('已取消');
-      },
-      fail: function (res) {
-        alert(JSON.stringify(res));
-      }
-    });
-    alert('已注册获取“发送给朋友”状态事件');
-  };
-
-    // 5 图片接口
-  // 5.1 拍照、本地选图
-  var images = {
-    localId: [],
-    serverId: []
-  };
-  document.querySelector('#chooseImage').onclick = function () {
-    wx.chooseImage({
-      success: function (res) {
-        images.localId = res.localIds;
-        alert('已选择 ' + res.localIds.length + ' 张图片');
-      }
-    });
-  };
-    // 5.2 图片预览
-  document.querySelector('#previewImage').onclick = function () {
-    wx.previewImage({
-      current: 'http://img5.douban.com/view/photo/photo/public/p1353993776.jpg',
-      urls: [
-        'http://img3.douban.com/view/photo/photo/public/p2152117150.jpg',
-        'http://img5.douban.com/view/photo/photo/public/p1353993776.jpg',
-        'http://img3.douban.com/view/photo/photo/public/p2152134700.jpg'
-      ]
-    });
-  };
-
     // 7.2 获取当前地理位置
   document.querySelector('#getLocation').onclick = function () {
     wx.getLocation({
