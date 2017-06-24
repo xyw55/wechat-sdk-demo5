b709611252ca3f3aac0ca9323309c4496a992165
xiayiwei <yiwei.xia@ele.me>
2017-06-24 17:23:53 +0800

    commit 21

--- config/wechat.cfg.js
+++ config/wechat.cfg.js
@@ -1,6 +1,6 @@
 module.exports = {
 	grant_type: 'client_credential',
-	appid: 'wxea41e4ef96cd0ac8',
+	appid: 'wx1d3765eb45497a18',
 	secret: 'uJBYD04TA1wGRmp4rA_GMaiLdIhuBUhPIpF3OT9QQvpe9PmixaJSsz3sU-aEq2Dc',
 	noncestr:'Wm3WZYTPz0wzccnW',
 	accessTokenUrl:'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=wx1d3765eb45497a18&corpsecret=uJBYD04TA1wGRmp4rA_GMaiLdIhuBUhPIpF3OT9QQvpe9PmixaJSsz3sU-aEq2Dc',
